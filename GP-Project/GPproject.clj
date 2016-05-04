faff7605473c8085b1805ee422a07e3ada6c2b6c
denningk <denningk18@mail.wlu.edu>
2016-05-04 12:06:49 -0400

    Initial Interface

--- GP-Project/GPproject.clj
+++ GP-Project/GPproject.clj
@@ -1,2 +1,21 @@
 (ns cs253.GPproject)
 
+; Initialization
+
+(defn rand-subtree
+  "This function generates a random subtree."
+  [terminal-set function-set]
+)
+
+    (defn rand-program
+      "This program generates a random program
+  using a max-depth and a primitive set."
+      [max-depth terminal-set function-set]
+      )
+
+
+    (defn gen-population
+      "This program generates a population of
+   randomly generated programs."
+      [num-pop max-depth terminal-set function-set]
+      )
