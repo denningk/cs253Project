c370de837a8978b5a79f06a94f4f853dab301a55
denningk <denningk18@mail.wlu.edu>
2016-05-04 12:17:42 -0400

    Update GPproject.clj

--- GP-Project/GPproject.clj
+++ GP-Project/GPproject.clj
@@ -1,20 +1,32 @@
 (ns cs253.GPproject)
 
+
 ; Initialization
 
+   
+  
 (defn rand-subtree
   "This function generates a random subtree."
   [terminal-set function-set]
-)
+  (let [rand-item (rand-nth (rand-nth '(terminal-set function-set)))]
+    (if (fn? rand-item )
+      '(rand-item (rand-nth terminal-set) (rand-nth terminal-set))
+      '(rand-item))))
+
+(rand-nth ('(2 3 4  x) '(+ - *)))
+
+(rand-subtree '(2 3 4  x) '(+ - *))
+  
 
 (defn rand-program
   "This program generates a random program
   using a max-depth and a primitive set."
   [max-depth terminal-set function-set]
-)
+  )
+
 
 (defn gen-population
   "This program generates a population of
-  randomly generated programs."
+   randomly generated programs."
   [num-pop max-depth terminal-set function-set]
-)
+  )
