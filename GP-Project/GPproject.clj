b6ff150e18b1c9562ad85fec852dd4bf35188643
denningk <denningk18@mail.wlu.edu>
2016-05-04 12:07:40 -0400

    Update GPproject.clj

--- GP-Project/GPproject.clj
+++ GP-Project/GPproject.clj
@@ -7,15 +7,14 @@
   [terminal-set function-set]
 )
 
-    (defn rand-program
-      "This program generates a random program
+(defn rand-program
+  "This program generates a random program
   using a max-depth and a primitive set."
-      [max-depth terminal-set function-set]
-      )
+  [max-depth terminal-set function-set]
+)
 
-
-    (defn gen-population
-      "This program generates a population of
-   randomly generated programs."
-      [num-pop max-depth terminal-set function-set]
-      )
+(defn gen-population
+  "This program generates a population of
+  randomly generated programs."
+  [num-pop max-depth terminal-set function-set]
+)
