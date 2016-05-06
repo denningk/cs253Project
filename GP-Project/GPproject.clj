25cf6efa830fc7b73ea0064771461cc8a18fc342
denningk <denningk18@mail.wlu.edu>
2016-05-06 11:32:35 -0400

    Update GPproject.clj

--- GP-Project/GPproject.clj
+++ GP-Project/GPproject.clj
@@ -9,36 +9,30 @@
   [terminal-set function-set]
   (list (rand-nth function-set) (rand-nth terminal-set) (rand-nth terminal-set)))
 
-
 (rand-frontier-tree '(2 3 4 x) '(+ - *))
-
 
 (defn rand-terminal
   "This function returns a random terminal from the terminal set."
   [terminal-set]
   (list (rand-nth terminal-set)))
-                                                                                  
-                                                        
+  
 (rand-terminal '(2 3 4 x))
 
 (defn full-tree
   "This function creates a random full-tree"
   [terminal-set function-set max-depth]
- (loop [depth max-depth]
-   (list (if (< depth max-depth)
-           (rand-nth function-set)
-           (rand-nth terminal-set))
-           (if (= depth max-depth)
-             (rand-nth terminal-set)
-             (recur (dec depth)))
-           (if (= depth max-depth)
-             (rand-nth terminal-set)
-             (recur (dec depth))))))
+  (list (rand-nth function-set)
+        (if (= 0 max-depth)
+          (rand-nth terminal-set)
+          (full-tree terminal-set function-set (dec max-depth)))
+        (if (= 0 max-depth)
+          (rand-nth terminal-set)
+          (full-tree terminal-set function-set (dec max-depth)))))
 
 
-(full-tree '(2 3 4 x) '(+ - *) 2)
+(full-tree '(2 3 5 x) '(+ - *) 1)
 
-  
+
 (defn grow-tree
   "This function generates a random tree."
   [terminal-set function-set]
@@ -61,4 +55,11 @@
   "This program generates a population of
    randomly generated programs."
   [num-pop max-depth terminal-set function-set]
-  )
+  (loop [pop []
+         num num-pop]
+    (if (= num 0)
+      pop
+      (recur (conj pop (full-tree terminal-set function-set max-depth))
+             (dec num)))))
+
+(gen-population 5 3 '(2 3 4 x) '(+ - *))
