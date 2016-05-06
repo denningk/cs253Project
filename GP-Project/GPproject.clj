6e19a99143a966b6c15721522f470cb6fe1f66a8
denningk <denningk18@mail.wlu.edu>
2016-05-06 17:26:01 -0400

    Added the grow-tree and ramped half and half funcs

--- GP-Project/GPproject.clj
+++ GP-Project/GPproject.clj
@@ -33,14 +33,27 @@
 (full-tree '(2 3 5 x) '(+ - *) 1)
 
 
-(defn grow-tree
-  "This function generates a random tree."
-  [terminal-set function-set]
-  (loop [tree '()]
-    (if (> (rand-int 0 100) 60)
-      )))
 
-(some #(= '+ %) '(+ - *))
+(defn grow-tree
+  "This function creates a random grow-tree"
+  [terminal-set function-set max-depth]
+  
+  (let [item (rand-nth (concat function-set terminal-set) )]
+          (if (some #(= item %) terminal-set)
+            item
+         (list item
+               (if (or (= 0 max-depth) (some #(= item %) terminal-set))
+                 (rand-nth terminal-set)
+                 (grow-tree terminal-set function-set (dec max-depth)))
+                      
+               (if (or (= 0 max-depth) (some #(= item %) terminal-set))
+                 (rand-nth terminal-set)
+                 (grow-tree terminal-set function-set (dec max-depth)))
+         ))))
+  
+(grow-tree '(2 3 4 5) '(+ * -) 2)
+
+
 (grow-tree '(2 3 4 x) '(+ - *))
   
 
@@ -63,6 +76,17 @@
              (dec num)))))
 
 (gen-population 5 3 '(2 3 4 x) '(+ - *))
+
+
+(defn make-population
+  "Makes a population of n number of program-trees of a random max-depth in range [2,6] 
+using terminal-set and function-set"
+  [n terminal-set function-set]
+  (list (take (* 0.5 n)(repeat (full-tree terminal-set function-set (rand-nth (range 2 6)))))
+        (take (* 0.5 n)(repeat (grow-tree terminal-set function-set (rand-nth (range 2 6)))))))
+
+
+(make-population 4 '(2 3 4 5) '(+ * -))
     
 
 (def test-cases {})
@@ -71,7 +95,7 @@
   "This function creates a map of functions with their
    respective error values."
   [population test-cases]
-  (map #((let [x 
+  (map #((let [x]))))
     
     
     
